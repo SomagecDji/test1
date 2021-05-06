1d6ae09a1af53562db95a18dd18f100d4e79fd38
SomagecDji <82517172+SomagecDji@users.noreply.github.com>
2021-05-06 12:41:39 +0000

    Update folder.py

--- custom_documents/models/folder.py
+++ custom_documents/models/folder.py
@@ -4,6 +4,5 @@ from odoo.exceptions import ValidationError
 class DocumentFolder(models.Model):
     _description = 'Document folder'
     _inherit = 'documents.folder'
-    admin_group_ids = fields.Many2many('res.groups',
-        string="Groupe d'écriture")
+    admin_group_ids = fields.Many2many('res.groups',  'documents_folder_admin_groups',string="Groupe d'écriture")
 
