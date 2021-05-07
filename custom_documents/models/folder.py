68858dba8d2f86e2b95517518848d2db52eaa9a8
SomagecDji <82517172+SomagecDji@users.noreply.github.com>
2021-05-07 12:47:43 +0000

    Add files via upload

--- custom_documents/models/folder.py
+++ custom_documents/models/folder.py
@@ -5,4 +5,16 @@ class DocumentFolder(models.Model):
     _description = 'Document folder'
     _inherit = 'documents.folder'
     admin_group_ids = fields.Many2many('res.groups',  'documents_folder_admin_groups',string="Groupe d'écriture")
+    def custom_groups(self):
+        folders=self.env['documents.folder'].search([])
+        for folder in folders:
+            if folder.parent_folder_id:
+                parent_folder_id=folder.parent_folder_id.id
+                parent_folder=self.env['documents.folder'].search(['id','=',parent_folder_id])
+                parent_folder.write({'read_group_ids':[(4, [group.id for group in folder.read_group_ids])]})
+                parent_folder.write({'read_group_ids':[(4, [group.id for group in folder.group_ids])]})
+        return()
+                
+                
+            
 
