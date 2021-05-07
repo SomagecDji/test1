ca71755e36e98d59751da38226b540dae5d15962
SomagecDji <82517172+SomagecDji@users.noreply.github.com>
2021-05-07 16:33:22 +0000

    Update folder.py

--- custom_documents/models/folder.py
+++ custom_documents/models/folder.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from odoo import models, fields, api, _
-from odoo.exceptions import ValidationError
+from odoo.exceptions import ValidationError, UserError
 class DocumentFolder(models.Model):
     _description = 'Document folder'
     _inherit = 'documents.folder'
@@ -12,9 +12,11 @@ class DocumentFolder(models.Model):
                 parent_folder_id=folder.parent_folder_id.id
                 parent_folder=self.env['documents.folder'].search([('id','=',parent_folder_id)])
                 if folder.read_group_ids:
+                    raise UserError(_(folder.read_group_ids))
                     parent_folder.write({'read_group_ids':[(4, [group.id for group in folder.read_group_ids])]})
                 if folder.group_ids:
                     parent_folder.write({'read_group_ids':[(4, [group.id for group in folder.group_ids])]})
+                    raise UserError(_(folder.group_ids))
         return()
             
 
